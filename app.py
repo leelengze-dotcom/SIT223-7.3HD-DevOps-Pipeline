739858b7e6ddc0ac2d9dfab057ce02ea9e5dc67f
leelengze <leelengze@gmail.com>
2026-09-24 16:03:13 +1000

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,5 @@ def message():
         "message": "Hello from the SIT223 Jenkins Pipeline!"
     })
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
